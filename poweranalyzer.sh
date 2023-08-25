3afae4eb06e4bb111bdd7397f35c1f20ff9c481d
ccouto <ccouto@ua.pt>
2023-08-25 15:29:06 +0100

    prior to release

--- poweranalyzer.sh
+++ poweranalyzer.sh
@@ -1,12 +1,57 @@
 #!/bin/bash
+version="0.0.1"
+
 LC_NUMERIC=C
 
 verbose=0
+
 #acc_line=1     #legacy code
+
+#in some systems there is a greater accuracy if we use laptop_mode to detect when charging has stopped
+#however, laptop_mode is not available in all systems, thus we make this test first
+detect_laptop_mode=$(journalctl -b 0 -t laptop_mode | tail -1 | grep laptop_mode)
+if [[ -z $detect_laptop_mode ]]; then
+    #echo "No laptop mode!"
+    use_laptop_mode=0
+else
+    #echo "We have laptop mode"
+    use_laptop_mode=1
+fi
 
 # Process the arguments
 while [ $# -gt 0 ]; do
     case "$1" in
+        --version)
+            echo "poweranalyzer $version"
+            exit 1
+            ;;
+        --help)
+            echo "poweranalyzer $version"
+            echo "A bash script for better battery estimation"
+            echo "ccouto <ccouto@ua.pt>"
+            echo ""
+            echo "This is the help, but there is not much I can do for you, just run me"
+            echo ""
+            echo "The output is as follows:"
+            echo ""
+            echo "Current battery			current battery available (in W*h)"
+            echo "Percentage			percentage of battery available"
+            echo "Running on battery for		time running on battery (minus suspend time, since boot)"  
+            echo "Current power usage		power usage from ACPI (it might not be available in your system)"
+            echo "Average power usage		average power consumption (calculated based on running on battery)"
+            echo "Time to empty			available time running on battery"
+            echo "Battery full-span		based on average, the full battery timespan"
+            echo ""
+            echo "Note: this script is still experimental, if you get strange readings, let the battery discharge slightly more and try again."
+            exit 1
+            #echo "version $version"
+            #echo "Carlos Couto"
+        ;;
+        #next option is to use laptop mode, currently not implemented
+        -l)
+            echo "disable using laptop mode"
+            use_laptop_mode=0
+            ;;
         -v)
             verbose=1
             ;;
@@ -36,8 +81,8 @@ cur_battery_percent=$(echo "$output" | grep "percentage:" | awk '{print $2}' | t
 cur_state=$(echo "$output" | grep "state:" | awk '{print $2}' | tr , .)
 
 # Check if cur_state is "charging"
-if [ "$cur_state" = "charging" ]; then
-    echo "Stopping script: Battery is charging"
+if [ "$cur_state" != "discharging" ]; then
+    echo "Stopping script: Battery is not discharging."
     exit 1  # Exit the script with a non-zero status code
 fi
 
@@ -65,7 +110,7 @@ done
 # Read the file into an array
 mapfile -t lines < "/var/lib/upower/$file"
 
-last_charging_line=""
+first_discharging_line=""
 bat_lastcharge=-1
 
 # Loop through the array in reverse
@@ -74,40 +119,50 @@ for ((i=${#lines[@]}-1; i>=0; i--)); do
     if [[ $line == *"	charging"* || $line == *"	fully-charged"* ]]; then
     #if [[ $line == *"	charging"* ]]; then
         if ((i < ${#lines[@]}-0)); then  # Note the change in index
-            last_charging_line_zero="${lines[$i+1]}"  # Retrieve the first line after last charging (it should be discharging)
-            last_charging_line="${lines[$i+2]}"  # For the measurements we take the second discharing line, this is because on some systems the charging thresholds might make some differences
+            first_discharging_line_zero="${lines[$i+1]}"  # Retrieve the first line after last charging (it should be discharging)
+            first_discharging_line="${lines[$i+1]}"  # For the measurements we take the second discharing line, this is because on some systems the charging thresholds might make some differences
             #note: we are getting some strange readings when we get fully-charged state
-            #for that particular case we consider now the last_charging_line_zero to be that line
+            #for that particular case we consider now the first_discharging_line_zero to be that line
             if [[ $line == *"	fully-charged"* ]]; then
-                last_charging_line_zero=$line
-                last_charging_line=$line
+                first_discharging_line_zero=$line
+                first_discharging_line=$line
             fi
-            timestamp1=$(echo $last_charging_line_zero | awk '{print $1}')
-            timestamp2=$(echo $last_charging_line | awk '{print $1}')
-            #echo "last line zero is $last_charging_line_zero"
+            timestamp1=$(echo $first_discharging_line_zero | awk '{print $1}')
+            timestamp2=$(echo $first_discharging_line | awk '{print $1}')
+            #echo "last line zero is $first_discharging_line_zero"
             #calculate the interval between these two values in seconds
-            #timestamp1=${last_charging_line_zero%% *}
-            #timestamp2=${last_charging_line%% *}
+            #timestamp1=${first_discharging_line_zero%% *}
+            #timestamp2=${first_discharging_line%% *}
             seconds_to_add=$((timestamp2 - timestamp1))
+            #echo "Seconds to add: $seconds_to_add"
             #echo "Line containing 'charging': $line"
             #echo "Second next line: $second_next_line"
-            #echo "there: $last_charging_line" 
+            #echo "there: $first_discharging_line" 
         else
             #echo "Line containing 'charging': $line (second last line)"
-            last_charging_line=$line  # No need to modify this line
+            first_discharging_line=$line  # No need to modify this line
         fi
         break
     fi
 
     if [[ $line == *"	discharging"* ]]; then
 
-        read -r start_bat bat_lastchargeread _ <<< "$line | tr , ."        
-        read -r start_bat bat_lastchargeread_before _ <<< "${lines[$i-1]} | tr , ."
+        read -r start_bat bat_lastchargeread _ <<< "$line | tr , ."    
+        read -r start_bat bat_lastchargeread_before thestatus _ <<< "${lines[$i-1]} | tr , ."
+        
+        if [[ $thestatus == *"unknown"* ]]; then
+            if [ $verbose -eq 1 ]; then
+                echo "we have ignored this line ${lines[$i-1]} because status is $thestatus" 
+            fi
+
+            read -r start_bat bat_lastchargeread_before _ <<< "${lines[$i-2]} | tr , ."
+        fi
+
         bat_lastchargeread=$(printf "%.0f" "$bat_lastchargeread")
         bat_lastchargeread_before=$(printf "%.0f" "$bat_lastchargeread_before")
         if (($bat_lastchargeread_before < $bat_lastchargeread )); then
-            last_charging_line_zero=$line
-            last_charging_line=$line            
+            first_discharging_line_zero=$line
+            first_discharging_line=$line            
             seconds_to_add=0
             #we got the line
             #sometimes if the charging occurs during suspend time only, the $file will not contain this information
@@ -118,8 +173,8 @@ for ((i=${#lines[@]}-1; i>=0; i--)); do
 
 done
 
-# Check if last_charging_line is empty if so, we need to exit otherwise results are wrong
-if [[ -z "$last_charging_line" ]]; then
+# Check if first_discharging_line is empty if so, we need to exit otherwise results are wrong
+if [[ -z "$first_discharging_line" ]]; then
     echo "We are stopping this script because there is not enough information about discharging state. Please wait until the battery is slightly discharged."
     exit 1
 fi
@@ -127,22 +182,23 @@ fi
 #next is some legacy code, to be removed later
 # first_discharging_line=$(grep "unknown" "/var/lib/upower/$file" | tail -n 1)
 # #echo "first is $first_discharging_line"
-# #last_charging_line=$(grep "	charging" "/var/lib/upower/$file" | tail -n 1)
-# #echo "$last_charging_line"
-#     if [ -z "$last_charging_line" ]; then  
+# #first_discharging_line=$(grep "	charging" "/var/lib/upower/$file" | tail -n 1)
+# #echo "$first_discharging_line"
+#     if [ -z "$first_discharging_line" ]; then  
 #     	first_discharging_line=$(grep "unknown" "/var/lib/upower/$file" | tail -n 2)
-#         last_charging_line="$first_discharging_line"
+#         first_discharging_line="$first_discharging_line"
 #     fi
 
-# Split last_charging_line by spaces and extract date and bat_lastcharge values
-read -r start_bat bat_lastcharge _ <<< "$last_charging_line | tr , ."
-read -r start_bat_4sus bat_lastcharge2 _ <<< "$last_charging_line_zero | tr , ."
+# Split first_discharging_line by spaces and extract date and bat_lastcharge values
+read -r start_bat bat_lastcharge _ <<< "$first_discharging_line | tr , ."
+read -r start_bat_4sus bat_lastcharge2 _ <<< "$first_discharging_line_zero | tr , ."
 
 # Echo the extracted values
 if [ $verbose -eq 1 ]; then
 #    echo "Verbose mode is enabled."
+echo "Current cycle:"
+echo "First Discharging Line: $first_discharging_line"
 echo "Last Discharging Line: $last_discharging_line"
-echo "Last Charging Line: $last_charging_line"
 echo "Extracted Date: $start_bat"
 echo "Extracted bat_lastcharge: $bat_lastcharge"
 fi
@@ -175,8 +231,15 @@ seconds_to_hms() {
     echo "${hours} hours, ${minutes} minutes, ${seconds} seconds"
 }
 
-# Get the first line from "journalctl" after the start of discharging state
-start_date=$(journalctl -o short-iso --since "@$start_bat" | head -n 1 | awk '{print $1}')
+if [ $use_laptop_mode -eq 1 ]; then
+    read -r start_date bat_lastcharge2 _ <<< "$(journalctl -o short-iso -b 0 -t laptop_mode | grep 'enabled, active$' | tail -1)"
+    #echo "new $start_bat_4sus"
+else
+    # Get the first line from "journalctl" after the start of discharging state
+start_date=$(journalctl -o short-iso --since "@$start_bat" -b 0 | head -n 1 | awk '{print $1}')
+fi
+
+
 
 #get the systemd status for sleeping states
 journalctl_output_suspend=$(journalctl -o short-iso --since "@$start_bat_4sus" -t systemd-sleep |grep -E "systemd-sleep.*sleep state")
@@ -225,15 +288,15 @@ estimated_empty_time=$(printf "%.0f" $estimated_empty_time)
 
 #previously we were calculating the battery full span based on the full battery capacity and average power usage (calculate_full=1)
 #however, this has shown to be somehow inconsistent, we now present the full battery span as the simple calculation of running time + time to empty (calculate_full=0)
-calculate_full=1
-if [ $calculate_full -eq 1 ]; then
+#calculate_full=1
+#if [ $calculate_full -eq 1 ]; then
     estimated_fullempty_time=$(awk -v er="$avr_power_usage" -v cb="$bat_full" 'BEGIN {print (cb/er) * 3600}')
     estimated_fullempty_time=$(printf "%.0f" "$estimated_fullempty_time")
-else
-    estimated_fullempty_time=$((estimated_empty_time + total_seconds + seconds_to_add))
-fi
+#else
+    estimated_cycleempty_time=$((estimated_empty_time + total_seconds + seconds_to_add))
+#fi
 # Show the result for running on bat
-echo "Running on battery for:		$(seconds_to_hms "$((total_seconds+seconds_to_add))")"
+echo "Running on battery for:		$(seconds_to_hms "$((total_seconds+seconds_to_add))") (since last boot)"
 
 # get the current power usage if the file exists
 if [ -e "/sys/class/power_supply/BAT0/power_now" ]; then
@@ -254,7 +317,8 @@ fi
 # Shows the results of calculations
 echo "Average power usage:		$avr_power_usage W"
 echo ""
-echo "Time to empty:		$(seconds_to_hms "estimated_empty_time")"
-echo "Battery full-span:	$(seconds_to_hms "estimated_fullempty_time")"
+echo "Time to empty:			$(seconds_to_hms "estimated_empty_time")"
+echo "Battery cycle duration:		$(seconds_to_hms "estimated_cycleempty_time")"
+echo "Battery full-span:		$(seconds_to_hms "estimated_fullempty_time")"
 
 
